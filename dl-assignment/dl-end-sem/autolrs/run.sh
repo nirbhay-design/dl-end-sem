170186a7c0989b79719e2ce6f52090c671f059b6
nirbhay-design <sharma.59@iitj.ac.in>
2022-04-26 18:59:08 +0530

    adding files

--- dl-assignment/dl-end-sem/autolrs/run.sh
+++ dl-assignment/dl-end-sem/autolrs/run.sh
@@ -5,6 +5,6 @@ MIN_LR=0.001
 MAX_LR=0.1
 PORT=12315
 
-python autolrs_server.py --min_lr $MIN_LR --max_lr $MAX_LR --port $PORT > $dir/server_log 2>&1 & echo $!
+python3 autolrs_server.py --min_lr $MIN_LR --max_lr $MAX_LR --port $PORT > $dir/server_log 2>&1 & echo $!
 
-CUDA_VISIBLE_DEVICES=0 python cifar10_example.py --port ${PORT}> $dir/client_log 2>&1
+CUDA_VISIBLE_DEVICES=0 python3 cifar100_example.py --port ${PORT}> $dir/client_log 2>&1
